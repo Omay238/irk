1ca55baffe1fc33de7e6bf92ac6c19eceb24705c
Omay238 <238maculo238@gmail.com>
2025-11-25 14:19:08 -0700

    docs and light stuff

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,19 @@ use tokio::io;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net;
 
+enum Message {
+    JOIN(String),
+    PART(String),
+}
+
+/// A simple IRC Client
 pub struct IRCClient {
     reader: io::ReadHalf<net::TcpStream>,
     writer: io::WriteHalf<net::TcpStream>,
 }
 
 impl IRCClient {
+    /// Create a new IRC Client to a specific server address
     pub async fn new(url: String) -> Self {
         let (reader, writer) = io::split(
             net::TcpStream::connect(url)
@@ -18,11 +25,14 @@ impl IRCClient {
         Self { reader, writer }
     }
 
+    /// Register a connection to the server
     pub async fn connect(&mut self, nick: String, name: String) {
         self.send_message(format!("NICK {nick}\r\n")).await;
-        self.send_message(format!("USER guest 0 * :{name}\r\n")).await;
+        self.send_message(format!("USER guest 0 * :{name}\r\n"))
+            .await;
     }
 
+    /// Begin the listening loop
     pub async fn listen(&mut self) {
         loop {
             let mut buf = [0u8; 512];
@@ -34,12 +44,11 @@ impl IRCClient {
                 Ok(n) => {
                     let parsed = String::from_utf8_lossy(&buf[..n]);
                     if parsed.starts_with("PING") {
-                        self.send_message(
-                            format!(
-                                "PONG {}\r\n",
-                                parsed.split(" ").last().expect("invalid ping")
-                            )
-                        ).await;
+                        self.send_message(format!(
+                            "PONG {}\r\n",
+                            parsed.split(" ").last().expect("invalid ping")
+                        ))
+                        .await;
                     }
                     print!("{}", String::from_utf8_lossy(&buf[..n]));
                 }
@@ -51,7 +60,16 @@ impl IRCClient {
         }
     }
 
-    async fn send_message(&mut self, content: String) {
+    /// Take a user space command and convert it to an IRC command and send it.
+    pub async fn send_command(&mut self, content: String) {}
+
+    /// Take an IRC command and convert it to an understandable Message enum
+    async fn parse_command(&mut self, content: String) -> Message {
+        Message::JOIN(String::new())
+    }
+
+    /// Helper function to easily send content to the server
+    pub async fn send_message(&mut self, content: String) {
         self.writer
             .write(content.as_bytes())
             .await
